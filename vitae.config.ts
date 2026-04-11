7a355e619d4ef40557129b69885ae105c51d9372
HomePC <stephenndichu006@gmail.com>
2026-04-11 11:19:53 -0700

    Fix: deploy to GitHub Pages

---
+++ vitae.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import checker from "vite-plugin-checker";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  base: "/Happybirthday-Wishes/", // ✅ Add this line
+  server: {
+    port: 3000,
+    host: true,
+    allowedHosts: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
